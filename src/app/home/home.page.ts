806a830d160fa613a6b14a8e9d0a2846de2a0014
JosueHuashuayo <jhuashuayo@unsa.edu.pe>
2025-04-23 11:18:15 -0500

    Ejemplo de Geolocalizacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common'; // Necesario para *ngIf y otras directivas comunes
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [IonicModule, CommonModule],  // Solo importa IonicModule y CommonModule
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  latitude: number | null = null;
+  longitude: number | null = null;
+
+  async getLocation() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+    } catch (error) {
+      console.error('Error getting location', error);
+    }
+  }
 }
+
